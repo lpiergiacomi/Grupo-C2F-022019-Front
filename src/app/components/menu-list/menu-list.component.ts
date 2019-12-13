39dc9e83fa3004406bb55a0906dc2fe99c5a283a
Lucas Piergiacomi <lg.piergiacomi@gmail.com>
2019-12-12 23:06:02 -0300

    datos de proveedor en tabla de menus

--- src/app/components/menu-list/menu-list.component.ts
+++ src/app/components/menu-list/menu-list.component.ts
@@ -1,5 +1,5 @@
-import {Component, ViewChild, OnInit} from '@angular/core';
-import {MatTableDataSource, MatSort, MatDialog} from '@angular/material';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatDialog } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MenuService } from '../../services/menu.service';
 import { Menu } from '../../model/menu';
@@ -7,23 +7,21 @@ import Swal from 'sweetalert2';
 import { ProviderService } from 'src/app/services/provider.service';
 import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
 import { Inject } from '@angular/core';
-import { Provider } from 'src/app/model/provider';
 
 @Component({
   selector: 'app-menu-list',
   templateUrl: './menu-list.component.html',
   styleUrls: ['./menu-list.component.css']
 })
-export class MenuListComponent implements OnInit{
-  //displayedColumns = ['name', 'description', 'price', 'minQuantity', 'minQuantityPrice', 'minQuantity2', 'minQuantityPrice2', 'reputationProvider', 'addressProvider', 'nameProvider', 'action'];
-  displayedColumns = ['name', 'description', 'price', 'minQuantity', 'minQuantityPrice', 'minQuantity2', 'minQuantityPrice2', 'action'];
+export class MenuListComponent implements OnInit {
+  displayedColumns = ['name', 'description', 'price', 'minQuantity', 'minQuantityPrice', 'minQuantity2', 'minQuantityPrice2', 'reputationProvider', 'addressProvider', 'nameProvider', 'action'];
   dataSource: MatTableDataSource<Menu>;
   menus: Array<Menu>;
   paginador: any;
 
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-constructor(@Inject(SESSION_STORAGE) private storage: StorageService, private menuService: MenuService, private router: Router, private providerService: ProviderService, private route: ActivatedRoute, public dialog: MatDialog){}
+  constructor(@Inject(SESSION_STORAGE) private storage: StorageService, private menuService: MenuService, private router: Router, private providerService: ProviderService, private route: ActivatedRoute, public dialog: MatDialog) { }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -37,22 +35,22 @@ constructor(@Inject(SESSION_STORAGE) private storage: StorageService, private me
   }
 
   reloadData() {
-    this.route.paramMap.subscribe( params => {
+    this.route.paramMap.subscribe(params => {
       let page: number = +params.get('page');
-      if (!page){ page = 0 }
+      if (!page) { page = 0 }
       this.menuService.getMenusList(page).subscribe(response => {
         (response.content as Menu[]).forEach((x) => {
           this.menus = response.content;
           this.paginador = response;
         });
         this.loadDataSource();
-      }, error => {});
+      }, error => { });
     });
   }
 
-  loadDataSource(){
+  loadDataSource() {
     this.dataSource = new MatTableDataSource(this.menus);
-      this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort;
   }
 
   deleteMenu(id: number) {
@@ -67,16 +65,16 @@ constructor(@Inject(SESSION_STORAGE) private storage: StorageService, private me
     }).then((result) => {
       if (result.value) {
         this.menuService.deleteMenu(id)
-        .subscribe(
-          data => {
-            this.reloadData();
-            Swal.fire(
-              'Eliminado',
-              'El proveedor fue eliminado correctamente.',
-              'success'
-            )
-      },
-      error => console.log(error));
+          .subscribe(
+            data => {
+              this.reloadData();
+              Swal.fire(
+                'Eliminado',
+                'El proveedor fue eliminado correctamente.',
+                'success'
+              )
+            },
+            error => console.log(error));
       }
     })
   }
@@ -88,20 +86,18 @@ constructor(@Inject(SESSION_STORAGE) private storage: StorageService, private me
   updateMenu(id: number) {
     this.router.navigate(['updateMenu/' + id]);
   }
-  
+
   purchase(id: number) {
     this.router.navigate(['purchase/' + id])
   }
 
-  esDuenioDeMenu(menu){
+  esDuenioDeMenu(menu) {
     return this.storage.get('providerId') == menu.idProvider;
   }
 
-  puedeComprar(menu){
+  puedeComprar(menu) {
     return this.storage.get('clientId') != undefined && this.storage.get('providerId') == undefined;
   }
 
-
-  
 }
 
